59877334c430b1e8bb87f9a945c30ba88a9396fd
anish102 <112316389+anish102@users.noreply.github.com>
2024-01-31 22:31:10 +0545

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -5,17 +5,14 @@ from . import views
 # Urls for different api endpoints
 urlpatterns = [
     path('', views.homepage),
-    path('users/create/', views.create_user, name='create_user'),
-    path('users/list/', views.list_all_users, name='list_all_users'),
-    path('users/<int:user_id>/', views.get_user_by_id, name='get_user'),
-    path('books/add/', views.add_new_book, name='add_book'),
-    path('books/list/', views.list_all_books, name='list_books'),
-    path('books/<int:book_id>/', views.get_book_by_id, name='get_book'),
-    path('books/<int:book_id>/details/',
-         views.assign_update_book_details, name='details'),
-    path('borrow/', views.borrow_book, name='borrow_book'),
-    path('return/<int:borrow_id>/', views.return_book, name='return_book'),
-    path('borrowed-books/', views.list_all_borrowed_books,
-         name='list_borrowed_books'),
-
+    path('api/users/create/', views.create_user, name='create_user'),
+    path('api/users/list/', views.list_all_users, name='list_all_users'),
+    path('api/users/<int:user_id>/', views.get_user_by_id, name='get_user'),
+    path('api/books/add/', views.add_new_book, name='add_book'),
+    path('api/books/list/', views.list_all_books, name='list_books'),
+    path('api/books/<int:book_id>/', views.get_book_by_id, name='get_book'),
+    path('api/books/<int:book_id>/details/',views.assign_update_book_details, name='details'),
+    path('api/borrow/', views.borrow_book, name='borrow_book'),
+    path('api/return/<int:borrow_id>/', views.return_book, name='return_book'),
+    path('api/borrowed-books/', views.list_all_borrowed_books,name='list_borrowed_books'),
 ]
